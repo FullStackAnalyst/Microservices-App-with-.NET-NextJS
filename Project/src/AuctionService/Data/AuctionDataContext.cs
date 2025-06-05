7976ef7c26bd4d9084edac3361c7b3d32859f9ef
FullStackAnalyst <ars.ict2021@gmail.com>
2025-06-05 17:29:44 +0600

    MSA-4

---
+++ Project/src/AuctionService/Data/AuctionDataContext.cs
@@ -0,0 +1,11 @@
+﻿using AuctionService.Entities.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace AuctionService.Data;
+
+public class AuctionDataContext(DbContextOptions<AuctionDataContext> options) : DbContext(options)
+{
+    public DbSet<Auction> Auctions { get; set; }
+
+    public DbSet<Item> Items { get; set; }
+}
--- Project/src/AuctionService/Program.cs
+++ Project/src/AuctionService/Program.cs
@@ -1,6 +1,11 @@
+using AuctionService.Data;
+using Microsoft.EntityFrameworkCore;
+
 WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
+builder.Services.AddDbContext<AuctionDataContext>(options => _ = options.UseNpgsql(builder.Configuration.GetConnectionString("")));
 
 builder.Services.AddControllers();
 
