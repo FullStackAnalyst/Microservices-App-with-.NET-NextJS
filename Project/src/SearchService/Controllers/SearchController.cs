0068003a9abc15f0192af87266198ec8a7993481
FullStackAnalyst <ars.ict2021@gmail.com>
2025-06-13 16:47:53 +0600

    MSA-7

--- Project/src/SearchService/Controllers/SearchController.cs
+++ Project/src/SearchService/Controllers/SearchController.cs
@@ -3,41 +3,76 @@ using MongoDB.Bson;
 using MongoDB.Driver;
 using MongoDB.Entities;
 using SearchService.Entities.Models;
+using SearchService.Entities.Requests;
 
 namespace SearchService.Controllers;
 [Route("[controller]")]
 [ApiController]
 public class SearchController : ControllerBase
 {
-    [HttpGet("search")]
-    public async Task<ActionResult<object>> SearchItems(string searchTerm, int pageNumber = 1, int pageSize = 5)
+    [Route("search")]
+    [HttpGet]
+    public async Task<ActionResult<object>> SearchItems([FromQuery] PagedSearchRequest request)
     {
         var collection = DB.Collection<Item>();
 
-        var filter = Builders<Item>.Filter.Empty;
+        var filters = new List<FilterDefinition<Item>>();
 
-        if (!string.IsNullOrWhiteSpace(searchTerm))
+        if (!string.IsNullOrWhiteSpace(request.SearchTerm))
         {
-            filter = Builders<Item>.Filter.Or(
-                Builders<Item>.Filter.Regex(x => x.Make, new BsonRegularExpression(searchTerm, "i")),
-                Builders<Item>.Filter.Regex(x => x.Model, new BsonRegularExpression(searchTerm, "i")),
-                Builders<Item>.Filter.Regex(x => x.Color, new BsonRegularExpression(searchTerm, "i"))
-            );
+            filters.Add(Builders<Item>.Filter.Or(
+                Builders<Item>.Filter.Regex(x => x.Make, new BsonRegularExpression(request.SearchTerm, "i")),
+                Builders<Item>.Filter.Regex(x => x.Model, new BsonRegularExpression(request.SearchTerm, "i")),
+                Builders<Item>.Filter.Regex(x => x.Color, new BsonRegularExpression(request.SearchTerm, "i"))
+            ));
         }
+
+        if (!string.IsNullOrWhiteSpace(request.Seller))
+        {
+            filters.Add(Builders<Item>.Filter.Eq(x => x.Seller, request.Seller));
+        }
+
+        if (!string.IsNullOrWhiteSpace(request.Winner))
+        {
+            filters.Add(Builders<Item>.Filter.Eq(x => x.Winner, request.Winner));
+        }
+
+        var currentTime = DateTime.UtcNow;
+        if (!string.IsNullOrWhiteSpace(request.FilterBy))
+        {
+            filters.Add(request.FilterBy.ToLower() switch
+            {
+                "finished" => Builders<Item>.Filter.Lt(x => x.AuctionEnd, currentTime),
+                "endingsoon" => Builders<Item>.Filter.And(
+                    Builders<Item>.Filter.Lt(x => x.AuctionEnd, currentTime.AddHours(6)),
+                    Builders<Item>.Filter.Gt(x => x.AuctionEnd, currentTime)
+                ),
+                _ => Builders<Item>.Filter.Gt(x => x.AuctionEnd, currentTime)
+            });
+        }
+
+        var filter = filters.Count > 0 ? Builders<Item>.Filter.And(filters) : Builders<Item>.Filter.Empty;
 
         var totalCount = await collection.CountDocumentsAsync(filter);
 
-        var items = await collection
-            .Find(filter)
-            .Sort(Builders<Item>.Sort.Ascending(x => x.Make))
-            .Skip((pageNumber - 1) * pageSize)
-            .Limit(pageSize)
+        var query = collection.Find(filter);
+
+        query = request.OrderBy?.ToLower() switch
+        {
+            "make" => query.Sort(Builders<Item>.Sort.Ascending(x => x.Make).Ascending(x => x.Model)),
+            "new" => query.Sort(Builders<Item>.Sort.Descending(x => x.CreatedAt)),
+            _ => query.Sort(Builders<Item>.Sort.Ascending(x => x.AuctionEnd))
+        };
+
+        var items = await query
+            .Skip(Math.Max(0, (request.PageNumber - 1) * request.PageSize))
+            .Limit(request.PageSize)
             .ToListAsync();
 
         return Ok(new
         {
             results = items,
-            pageCount = (int)Math.Ceiling((double)totalCount / pageSize),
+            pageCount = (int)Math.Ceiling((double)totalCount / request.PageSize),
             totalCount
         });
     }
---
+++ Project/src/SearchService/Entities/Requests/PagedSearchRequest.cs
@@ -0,0 +1,18 @@
+﻿namespace SearchService.Entities.Requests;
+
+public class PagedSearchRequest
+{
+    public string? SearchTerm { get; set; }
+
+    public int PageNumber { get; set; } = 1;
+
+    public int PageSize { get; set; } = 5;
+
+    public string? Seller { get; set; }
+
+    public string? Winner { get; set; }
+
+    public string? OrderBy { get; set; }
+
+    public string? FilterBy { get; set; }
+}
